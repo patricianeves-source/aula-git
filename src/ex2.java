7afa560501ab7bedbb30ec5dad8f8ffef74ef9ef
patricianeves <patricia.neves@edu.senai.br>
2026-03-18 16:17:43 -0300

    feat(ex02): pedido em restaurante

---
+++ src/ex2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ex2 {
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        IO.println(" Restaurante ");
+
+        IO.println("registro de pedidos");
+
+        IO.println("Nome do cliente");
+
+        String nome = sc.nextLine();
+
+        IO.println("nome do prato");
+
+        String prato = sc.nextLine();
+
+        IO.println("valor do prato");
+
+        int valor = sc.nextInt();
+
+        IO.println("pedido final");
+
+        IO.println("nome do cliente " + nome );
+
+        IO.println("nome do prato " + prato );
+
+        IO.println("valor do prato " + valor );
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
