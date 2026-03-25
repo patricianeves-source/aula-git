7aec93ac4d79d620e584d5ab82bd59bfe5de684d
patricianeves <patricia.neves@edu.senai.br>
2026-03-25 17:11:13 -0300

    feat:ex10

---
+++ src/ex10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex10 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+         int total = 300;
+
+        IO.println("quantas páginas você leu hoje?");
+
+        int paginas = sc.nextInt();
+
+        int falta = total - paginas;
+        IO.println("Faltam " + falta +" paginas");
+    }
+}
