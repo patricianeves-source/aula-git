c73b36d47fe227e61b1e25aee83009fb66595895
patricianeves <patricia.neves@edu.senai.br>
2026-03-25 15:10:22 -0300

    feat: ex8

--- src/ex8.java
+++ src/ex8.java
@@ -1,2 +1,51 @@
+import java.util.Scanner;
+
 public class ex8 {
+
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int restrição, idade = 18;
+
+
+        IO.println("Quantos anos você tem?");
+
+        restrição = sc.nextInt();
+
+        if (restrição >= idade) {
+
+            IO.println("você pode entrar");
+
+            IO.println("você tem" + (idade >= 18) + "de idade");
+
+        }else{
+
+            IO.println(" Você não pode entrar ");
+
+            IO.println(" Você não tem" + (idade >= 18) + "  idade necessaria ");
+
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
